e415ab1dac9216cfbc44a211ee49e8cc12446b11
karla galvez <karla.galvez.m@hotmail.com>
2019-06-06 20:46:21 -0500

    subiendoserverjs

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const app = express()
 const port = 3000
 
 app.get('/', function (req, res) {
-res.send('Hello World!')
+res.send("contestando con el metodo get")
 })
 
 app.listen(port, function () { 
 console.log(`Example app listening on port ${port}!`)
 })
+
+app.post('/', function (req, res) {
+    res.send("contestando con el metodo post")
+    })
+
+    app.put('/', function (req, res) {
+        res.send("contestando con el metodo put")
+        })
+
+        app.delete('/', function (req, res) {
+            res.send("contestando con el metodo delete")
+            })
 
